01e1bc157fcf2eaf546a533f7d4ad4dc42d91f38
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-30 22:54:21 +0800

    add new route

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
     
+@app.route("/fazlur")
+def salvador():
+    return "Hello, Fazlur!"
+    
 if __name__ == "__main__":
     app.run(debug=True)
